b0bcc77c3e9ffc20e480dc996322c513fd0d9440
zakariaattouni <zakariaattouni@gmail.com>
2026-05-26 15:56:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
+self.addEventListener("install", e => self.skipWaiting());
 
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+
+self.addEventListener("fetch", e => e.respondWith(fetch(e.request)));
