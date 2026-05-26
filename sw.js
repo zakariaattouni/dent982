55ce6a5c2716d66a6342a63fd1c0f808c0ea034b
zakariaattouni <zakariaattouni@gmail.com>
2026-05-26 21:05:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-self.addEventListener("install", e=>{
-self.skipWaiting();
+const CACHE_NAME = 'readaid-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
