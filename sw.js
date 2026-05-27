940962cd04304a5d3267258b2542a4e51d6c0e2c
zakariaattouni <zakariaattouni@gmail.com>
2026-05-27 08:37:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'readaid-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
